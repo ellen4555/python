4e6118b240789e11a86689195e5ef1bdec8732e9
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2024-10-01 15:00:40 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,7 @@
+name=input('enter your name')
+roll_number=int(input("enter your roll no"))
+roll_number=roll_number+1
+print("name of the student:,name")
+print("roll_number")
+cgpa=float(inout("enter your name"))
+percentage_mark=cgpa
