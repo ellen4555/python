f1443c98b3d373ae9012ebaf4a5475032e648d26
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2024-10-08 15:04:49 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,15 @@
+
+'''
+Author : ELLEN JOY
+Date : 8-10-2024
+Program : strings
+Version : 1.1
+'''
+first_name=input("Enter the first name:")
+last_name=input("Enter the last name:")
+full_name=first_name+" "+last_name
+print(full_name)
+length=len(first_name)
+extracted_first_name=full_name[:length]
+print(extracted_first_name)
+
