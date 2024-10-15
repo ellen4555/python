3b813c1ec58b7071a7c37c30703ca96cedf824fe
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2024-10-15 15:59:20 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,14 @@
+'''
+author: Ellen joy
+date: 15/10/2024
+program for generating n odd numbers
+'''
+
+
+limit=int(input("Enter the limit:"))
+odd_number=n
+count=0
+while count<limit:
+    print(odd_number,"\n",end="")
+    count+=1
+    odd_number+=2
