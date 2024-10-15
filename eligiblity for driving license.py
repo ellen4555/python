867348839dce76c7d8ca0149196bd57c840928ca
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2024-10-15 14:42:54 +0530

    Add files via upload

---
+++ eligiblity for driving license.py
@@ -0,0 +1,12 @@
+'''
+Author : ellen joy
+Date: 15-10-292
+python program for checking eligiblity of a person for driving license
+'''
+
+
+age=int(input("Enter your age:"))
+if(age>=16):
+    print("Eligible for driving license")
+else:
+    print("Not eligible for driving license")
