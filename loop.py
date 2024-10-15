726606a88ce9665d9b93b39b9f5c748e0b4ed347
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2024-10-15 16:00:52 +0530

    Add files via upload

--- loop.py
+++ loop.py
@@ -6,9 +6,9 @@ program for generating n odd numbers
 
 
 limit=int(input("Enter the limit:"))
-odd_number=n
+odd_number=1
 count=0
 while count<limit:
-    print(odd_number,"\n",end="")
+    print(odd_number,"\t",end="")
     count+=1
     odd_number+=2
