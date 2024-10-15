d3f96ac00f0a2f248ad87605ba906b9f309523be
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2024-10-15 14:05:58 +0530

    Add files via upload

---
+++ if else condition.py
@@ -0,0 +1,10 @@
+'''
+Author : ellen joy
+Date: 15-10-2924
+python progra to check whether the given number is positive or not
+'''
+number=int(input("Enter a number:"))
+if number>0:
+    print("The given number:",number,"is positive")
+else:
+     print("The given number:",number,"is negative")
