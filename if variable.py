11a860e2c762408c2bb1a6cd67b825434333b3c5
ellen4555 <ellenjoy2028@cs.sjcetpalai.ac.in>
2024-10-15 14:43:33 +0530

    Add files via upload

---
+++ if variable.py
@@ -0,0 +1,14 @@
+
+
+
+num1=int(input("Enter num1:"))
+num2=int(input("Enter num2:"))
+if num1>num2:
+    print("num1 is greater than num2")
+elif (num1<num2):
+    print("num2 is greater than num1")
+else:
+    print("The two numbers are equal")
+
+
+
